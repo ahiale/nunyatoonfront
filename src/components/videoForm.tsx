844cdaf60a4bbe23e0d09769abef98db4db69c0e
ahiale <beccalieben@gmail.com>
2024-07-31 03:17:02 +0000

    uploadvideo

--- src/components/videoForm.tsx
+++ src/components/videoForm.tsx
@@ -1,5 +1,10 @@
-import { useState } from 'react';
-import { FaFileAlt, FaTags, FaInfoCircle, FaPlay, FaCalendar, FaTv } from 'react-icons/fa';
+import { SetStateAction, useEffect, useState } from 'react';
+import { FaFileAlt, FaTags, FaInfoCircle, FaPlay } from 'react-icons/fa';
+
+interface Categorie {
+  id: string;
+  titre: string;
+}
 
 interface VideoFormProps {
   onClose: () => void;
@@ -7,12 +12,46 @@ interface VideoFormProps {
 
 const VideoForm: React.FC<VideoFormProps> = ({ onClose }) => {
   const [videoType, setVideoType] = useState('movie');
+  const [videoSource, setVideoSource] = useState('');
   const [videoFile, setVideoFile] = useState<File | null>(null);
-  const [seasonName, setSeasonName] = useState('');
-  const [seriesName, setSeriesName] = useState('');
+  const [videoLink, setVideoLink] = useState('');
   const [title, setTitle] = useState('');
-  const [category, setCategory] = useState('');
   const [description, setDescription] = useState('');
+  const [couverture, setCouverture] = useState<File | null>(null);
+  const [categories, setCategories] = useState<Categorie[]>([]);
+  const [category, setCategory] = useState('');
+
+  const handleCategoryChange = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setCategory(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchallCategories = async () => {
+      try {
+        const categoriesResponse = await fetch(
+          "http://127.0.0.1:8000/categorie/allCategories",
+          {
+            method: "GET",
+          }
+        );
+
+        if (!categoriesResponse.ok) {
+          throw new Error(
+            `Erreur lors de la récupération des categories: ${categoriesResponse.statusText}`
+          );
+        }
+
+        const categorieData: Categorie[] = await categoriesResponse.json();
+        setCategories(categorieData);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données:", error);
+      }
+    };
+
+    fetchallCategories();
+  }, []);
+
+
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
@@ -21,41 +60,81 @@ const VideoForm: React.FC<VideoFormProps> = ({ onClose }) => {
     }
   };
 
+  const handleCouvertureChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setCouverture(e.target.files[0]);
+      console.log(e.target.files[0].name);  // Affiche le titre du fichier dans la console
+    }
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log(videoFile)
 
-    if (!videoFile) {
+    
+
+    if (videoSource === '4' && videoFile ==null ) {
       console.error("Aucun fichier sélectionné");
       return;
     }
 
-//     const formData = new FormData();
-//   formData.append("file", videoFile);
-// formData.append("titre", title);
-// formData.append("description", description);
-// formData.append("duree", duration);
-// formData.append("type_video", videoType);
-// formData.append("admin_id", adminId);
-// if (seasonName) formData.append("saison_id", seasonName);
-// if (categoryId) formData.append("categorie_id", categoryId);
+    const formData = new FormData();
+    formData.append("file", videoFile!);
+    
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/upload/", {
+
+      const response = await fetch("http://127.0.0.1:8000/video/upload/", {
         method: "POST",
-        // body: formData,
+        body:formData,
       });
+      const dataVideoUpload= await response.json()
+
 
       if (!response.ok) {
         throw new Error(`Erreur: ${response.statusText}`);
       }
 
-      const result = await response.json();
-      console.log(result); // Affiche la réponse du serveur
+
+      const formDataTwo = new FormData();
+
+      console.log(couverture)
+      console.log(title)
+      console.log(description)
+      console.log(videoSource)
+      console.log(category)
+
+      formDataTwo.append("couverture", couverture!);
+      formDataTwo.append("titre", title!);
+      formDataTwo.append("description", description!);
+      formDataTwo.append("video_source", videoSource!);
+      formDataTwo.append("url", dataVideoUpload.url);
+      formDataTwo.append("categorie_id", category!);
+      formDataTwo.append("duree", "1h");
+      formDataTwo.append("type_video", "1");
+      
+      const responsetwo = await fetch("http://127.0.0.1:8000/video/createVideo", {
+        method: "POST",
+        body:formDataTwo,
+      });
+
+      if (!responsetwo.ok) {
+        throw new Error(`Erreur: ${response.statusText}`);
+      }
+
+      const resulttwo = await responsetwo.json();
+
+      console.log(resulttwo); // Affiche la réponse du serveur
+
       onClose(); // Ferme le formulaire après la soumission
     } catch (error) {
       console.error("Erreur lors du téléchargement du fichier:", error);
     }
   };
+
+  // function setCouverture(value: string): void {
+  //   throw new Error('Function not implemented.');
+  // }
 
   return (
     <div className="fixed inset-0 bg-gray-900 bg-opacity-70 flex justify-center items-center overflow-y-auto">
@@ -66,40 +145,123 @@ const VideoForm: React.FC<VideoFormProps> = ({ onClose }) => {
             <span className="sr-only">Fermer</span>
           </button>
         </div>
-        <div className="border-dashed border-2 border-gray-600 p-4 rounded-md text-center h-30 flex flex-col justify-center">
-          <input type="file" className="hidden" id="file-upload" onChange={handleFileChange} required />
-          <label htmlFor="file-upload" className="cursor-pointer">
-            <div className="flex flex-col items-center justify-center">
-              <svg className="w-8 h-8 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-4-4V4a4 4 0 014-4h5.586a1 1 0 01.707.293l7.414 7.414a1 1 0 01.293.707V12a4 4 0 01-4 4H7z"></path>
-              </svg>
-              <p className="mt-2 text-sm text-gray-500">Glissez les fichiers ici pour les télécharger </p>
-            </div>
-          </label>
+        <div>
+          <select
+            value={videoSource}
+            onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setVideoSource(e.target.value)}
+            className="mt-1 block w-full p-2 border-2 border-gray-400 rounded-md bg-white text-black focus:border-purple-600 focus:outline-none"
+            required
+          >
+            <option value="" disabled>Choisissez la source de la vidéo</option>
+            <option value="1">YouTube</option>
+            <option value="2">Dailymotion</option>
+            <option value="3">ANIMESAMA</option>
+            <option value="4">Upload</option>
+          </select>
         </div>
-        <div className="flex space-x-4">
-          <div className="relative w-1/2">
-            <FaFileAlt className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+        {videoSource === '1' && (
+          <div className="relative">
+            <FaPlay className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
             <input
               type="text"
-              placeholder="Titre de la Vidéo"
+              placeholder="Lien YouTube"
               className="mt-1 block w-full pl-8 p-2 border-2 border-gray-400 rounded-md bg-white text-black focus:border-purple-600 focus:outline-none"
-              value={title}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
+              value={videoLink}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVideoLink(e.target.value)}
               required
             />
           </div>
-          <div className="relative w-1/2">
-            <FaTags className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+        )}
+        {videoSource === '2' && (
+          <div className="relative">
+            <FaPlay className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
             <input
               type="text"
-              placeholder="Catégorie"
+              placeholder="Lien Dailymotion"
               className="mt-1 block w-full pl-8 p-2 border-2 border-gray-400 rounded-md bg-white text-black focus:border-purple-600 focus:outline-none"
+              value={videoLink}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVideoLink(e.target.value)}
+              required
+            />
+          </div>
+        )}
+        {videoSource === '3' && (
+          <div className="relative">
+            <FaPlay className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Lien ANIMESAMA"
+              className="mt-1 block w-full pl-8 p-2 border-2 border-gray-400 rounded-md bg-white text-black focus:border-purple-600 focus:outline-none"
+              value={videoLink}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVideoLink(e.target.value)}
+              required
+            />
+          </div>
+        )}
+        {videoSource === '4' && (
+          <div className="border-dashed border-2 border-gray-600 p-4 rounded-md text-center h-30 flex flex-col justify-center">
+     
+          <input
+            type="file"
+            placeholder="couverture"
+            className="mt-1 block w-full pl-8 p-2 border-2 border-gray-400 rounded-md bg-white text-black focus:border-purple-600 focus:outline-none"
+            required
+            onChange={handleFileChange}
+          />
+        
+            {/* <label htmlFor="file-upload" className="cursor-pointer">
+              <div className="flex flex-col items-center justify-center">
+                <svg className="w-8 h-8 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-4-4V4a4 4 0 014-4h5.586a1 1 0 01.707.293l7.414 7.414a1 1 0 01.293.707V12a4 4 0 01-4 4H7z"></path>
+                </svg>
+                <p className="mt-2 text-sm text-gray-500">Glissez les fichiers ici pour les télécharger </p>
+              </div>
+            </label> */}
+          </div>
+        )}
+        <div className="relative">
+          <FaFileAlt className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+          <input
+            type="text"
+            placeholder="Titre de la Vidéo"
+            className="mt-1 block w-full pl-8 p-2 border-2 border-gray-400 rounded-md bg-white text-black focus:border-purple-600 focus:outline-none"
+            value={title}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="relative">
+          <FaFileAlt className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+          <input
+            type="file"
+            placeholder="couverture"
+            className="mt-1 block w-full pl-8 p-2 border-2 border-gray-400 rounded-md bg-white text-black focus:border-purple-600 focus:outline-none"
+            onChange={handleCouvertureChange}
+            required
+          />
+        </div>
+
+        <div className="relative">
+          <FaTags className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+          <select
               value={category}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCategory(e.target.value)}
-              required
-            />
-          </div>
+              onChange={handleCategoryChange}
+            className="mt-1 block w-full pl-8 p-2 border-2 border-gray-400 rounded-md bg-white text-black focus:border-purple-600 focus:outline-none text-black"
+            required
+          >
+            <option value="" disabled>Choisissez une catégorie</option>
+            <div></div>
+            {categories.map((category, index) => (
+              <option
+               key={index} 
+               value={category.id}>
+              
+                {category.titre} 
+                
+                </option>
+            ))}
+          </select>
+
         </div>
         <div className="relative">
           <FaInfoCircle className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
@@ -112,42 +274,6 @@ const VideoForm: React.FC<VideoFormProps> = ({ onClose }) => {
             required
           />
         </div>
-        <div>
-          <select
-            value={videoType}
-            onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setVideoType(e.target.value)}
-            className="mt-1 block w-full p-2 border-2 border-gray-400 rounded-md bg-white text-black focus:border-purple-600 focus:outline-none"
-          >
-            <option value="movie">Film</option>
-            <option value="episode">Épisode</option>
-          </select>
-        </div>
-        {videoType === 'episode' && (
-          <>
-            <div className="relative">
-              <FaCalendar className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-              <input
-                type="text"
-                placeholder="Nom de la Saison"
-                className="mt-1 block w-full pl-8 p-2 border-2 border-gray-400 rounded-md bg-white text-black focus:border-purple-600 focus:outline-none"
-                value={seasonName}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSeasonName(e.target.value)}
-                required
-              />
-            </div>
-            <div className="relative">
-              <FaTv className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-              <input
-                type="text"
-                placeholder="Nom de la Série"
-                className="mt-1 block w-full pl-8 p-2 border-2 border-gray-400 rounded-md bg-white text-black focus:border-purple-600 focus:outline-none"
-                value={seriesName}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSeriesName(e.target.value)}
-                required
-              />
-            </div>
-          </>
-        )}
         <div className="flex justify-end mt-4">
           <button type="submit" className="px-4 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700">Publier</button>
           <button type="button" className="px-4 py-2 bg-red-600 text-gray-300 rounded-lg ml-2 hover:bg-red-700" onClick={onClose}>Annuler</button>
